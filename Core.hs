8adf046cc950aad18458415a1e591b90ad16c3d1
Stephan Schiffels <stephan.schiffels@mac.com>
2014-10-23 06:37:17 -0700

    added new command line option interface, implemented Model checks in getProb

--- Core.hs
+++ Core.hs
@@ -1,8 +1,9 @@
-module Core (defaultTimes, getProb) where
+module Core (defaultTimes, getProb, Join) where
 
 import Math.Combinatorics.Exact.Binomial (choose)
 import Control.Monad.State (State, get, put, execState)
-    
+import Data.List (sortBy)
+
 type Join = (Double, Int, Int, Double) -- t, k, l, N
 
 data CoalState = CoalState {
@@ -18,31 +19,38 @@ data ModelCoalState = ModelCoalState {
     mcsCoalState :: CoalState
 } deriving (Show)
 
-updateA :: Double -> [Double] -> [Double] -> [Double]
-updateA deltaT = zipWith go where
-    go lambdaK aK = aK * exp(-0.5 * (aK - 1) * lambdaK * deltaT)
+getProb :: [Double] -> [Double] -> [Join] -> Double -> [Int] -> [Int] -> Either String Double
+getProb timeSteps lambda joins theta nVec config = do
+    let sortedJoins = sortBy (\(t1,_,_,_) (t2,_,_,_) -> t1 `compare` t2) joins
+        lambda' = if length lambda > 0 then lambda else replicate (length nVec) 1.0
+        initState = ModelCoalState 0.0 sortedJoins lambda' (makeInitCoalState nVec config)
+        finalState = mcsCoalState $ execState (mapM_ singleStep timeSteps) initState
+    checkLengths nVec config lambda'
+    checkPopSizes lambda sortedJoins
+    checkJoins sortedJoins (length nVec)
+    return $ csD finalState * theta * fromIntegral (product $ zipWith choose nVec config)
+  where
+    checkLengths n m l =
+        if length n /= length m || length n /= length l
+            then Left "nVec, mVec, lambda have incompatible lengths"
+            else Right ()
+    checkPopSizes lambda joins =
+        if any (\l -> l < 0.001 || l > 100.0) (lambda ++ map (\(_,_,_,l) -> l) joins)
+            then Left "Population sizes must be between 0.001 and 100.0"
+            else Right ()
+    checkJoins [] nrPops = Right ()
+    checkJoins ((_,k,l,_):rest) nrPops =
+        if k == l || any (\(_,k',l',_) -> k' == l || l' == l) rest || k >= nrPops || l >= nrPops
+            then Left "Illegal joins"
+            else checkJoins rest nrPops
+        
+makeInitCoalState :: [Int] -> [Int] -> CoalState
+makeInitCoalState nVec config = 
+    let a = zipWith (\n m -> fromIntegral (n - m)) nVec config
+        b = [take m [0, 0..] ++ [1.0] | m <- config]
+    in  CoalState a b 0.0
 
-updateBk :: Double -> Double -> Double -> [Double] -> [Double] 
-updateBk deltaT lambdaK aK bK = zipWith3 go [0..] bK (tail bK ++ [0.0])
-    where go i bKi bKi' = bKi * exp(-(i * (i - 1) / 2 * lambdaK + i * aK * lambdaK) * deltaT)
-                          + bKi' * (1.0 - exp(-(i * (i + 1)) / 2.0 * lambdaK * deltaT))
-
-updateB :: Double -> [Double] -> [Double] -> [[Double]] -> [[Double]]
-updateB deltaT = zipWith3 (updateBk deltaT)
-
-updateD :: Double -> [[Double]] -> Double -> Double
-updateD deltaT b d = 
-    let kn = length b in  d + deltaT * sum (map go [0..(kn - 1)])
-    where go k = if length (b!!k) == 1 then 0.0 else product (update k (b!!k!!1) $ map head b)
-
-updateCoalState :: Double -> [Double] -> CoalState -> CoalState
-updateCoalState deltaT lambda coalState =    
-    let aNew = updateA deltaT lambda (csA coalState)
-        bNew = updateB deltaT lambda (csA coalState) (csB coalState)
-        dNew = updateD deltaT (csB coalState) (csD coalState)
-    in  CoalState aNew bNew dNew
-
-singleStep :: Double -> State ModelCoalState ModelCoalState
+singleStep :: Double -> State ModelCoalState ()
 singleStep nextTime = do
     ModelCoalState t joins lambda coalState <- get
     let (t', k, l, n) = if null joins then (1.0/0.0, 0, 0, 0.0) else head joins
@@ -57,14 +65,32 @@ singleStep nextTime = do
                  c = updateCoalState deltaT lambda coalState
              in  ModelCoalState nextTime joins lambda c
     put ms
-    return ms
-    
 
-update :: Int -> a -> [a] -> [a]
-update i val vec = let (x, y:ys) = splitAt i vec in x ++ (val:ys)
+updateCoalState :: Double -> [Double] -> CoalState -> CoalState
+updateCoalState deltaT lambda coalState =    
+    let aNew = updateA deltaT lambda (csA coalState)
+        bNew = updateB deltaT lambda (csA coalState) (csB coalState)
+        dNew = updateD deltaT (csB coalState) (csD coalState)
+    in  CoalState aNew bNew dNew
 
-delete :: Int -> [a] -> [a]
-delete i vec = let (x, y:ys) = splitAt i vec in x ++ ys
+updateA :: Double -> [Double] -> [Double] -> [Double]
+updateA deltaT = zipWith go where
+    go lambdaK aK = aK * exp(-0.5 * (aK - 1) * lambdaK * deltaT)
+
+updateB :: Double -> [Double] -> [Double] -> [[Double]] -> [[Double]]
+updateB deltaT = zipWith3 (updateBk deltaT)
+
+updateBk :: Double -> Double -> Double -> [Double] -> [Double] 
+updateBk deltaT lambdaK aK bK = zipWith3 go [0..] bK (tail bK ++ [0.0])
+  where
+    go i bKi bKi' = bKi * exp(-(i * (i - 1) / 2 * lambdaK + i * aK * lambdaK) * deltaT)
+                              + bKi' * (1.0 - exp(-(i * (i + 1)) / 2.0 * lambdaK * deltaT))
+
+updateD :: Double -> [[Double]] -> Double -> Double
+updateD deltaT b d =
+    let kn = length b in  d + deltaT * sum (map go [0..(kn - 1)])
+  where
+    go k = if length (b!!k) == 1 then 0.0 else product (update k (b!!k!!1) $ map head b)
 
 performJoin :: Int -> Int -> CoalState -> CoalState
 performJoin k l state =
@@ -80,12 +106,14 @@ performJoin k l state =
         new_b' = update l new_b_l new_b
     in  CoalState new_a' new_b' (csD state)
 
+update :: Int -> a -> [a] -> [a]
+update i val vec = let (x, y:ys) = splitAt i vec in x ++ (val:ys)
+
 joinProbs :: [Double] -> [Double] -> Int -> Double
 joinProbs bVec1 bVec2 i = sum $ zipWith (*) (take' (i + 1) bVec1) (reverse . take' (i + 1) $ bVec2)
     where take' i vec = take i (vec ++ repeat 0)
 
-getTimeStep :: Double -> Int -> Double -> Int -> Double
-getTimeStep alpha nr_steps tMax i = alpha * exp(fromIntegral i / fromIntegral nr_steps * log(1.0 + tMax / alpha)) - alpha
+defaultTimes = getTimeSteps 20000.0 400 20.0
 
 getTimeSteps :: Double -> Int -> Double -> [Double]
 getTimeSteps n0 lingen tMax = map (getTimeStep alpha nr_steps tMax) [1..nr_steps]
@@ -93,17 +121,9 @@ getTimeSteps n0 lingen tMax = map (getTimeStep alpha nr_steps tMax) [1..nr_steps
           alpha    = fromIntegral lingen / (2.0 * n0)
           nr_steps = floor $ log(1.0 + tMax / alpha) / log(1.0 + tMin / alpha)
 
-defaultTimes = getTimeSteps 20000.0 400 20.0
+getTimeStep :: Double -> Int -> Double -> Int -> Double
+getTimeStep alpha nr_steps tMax i =
+    alpha * exp(fromIntegral i / fromIntegral nr_steps * log(1.0 + tMax / alpha)) - alpha
 
-makeInitCoalState :: [Int] -> [Int] -> CoalState
-makeInitCoalState nVec config = 
-    let a = zipWith (\n m -> fromIntegral (n - m)) nVec config
-        b = [take m [0, 0..] ++ [1.0] | m <- config]
-    in  CoalState a b 0.0
-
-
-getProb :: [Double] -> [Double] -> [Join] -> Double -> [Int] -> [Int] -> Double
-getProb timeSteps lambda joins theta nVec config =
-    let initState = ModelCoalState 0.0 joins lambda (makeInitCoalState nVec config)
-        finalState = mcsCoalState $ execState (mapM_ singleStep timeSteps) initState
-    in  csD finalState * theta * fromIntegral (product $ zipWith choose nVec config)
+-- delete :: Int -> [a] -> [a]
+-- delete i vec = let (x, y:ys) = splitAt i vec in x ++ ys
--- Main.hs
+++ Main.hs
@@ -1,26 +1,77 @@
-import Core (defaultTimes, getProb)
+import Core (defaultTimes, getProb, Join)
 import System.Environment (getArgs)
 import Data.List.Split (splitOn)
 import Data.Monoid (mempty)
-import Control.Applicative ((<$>), (<*>))
+import Control.Monad (liftM, when)
+import Control.Applicative ((<$>), (<*>), pure)
 import qualified Options.Applicative as OP
+import Data.Monoid ((<>))
+import System.Exit (exitFailure)
 
-data MyOpts = MyOpts {
-    nVecStr :: String,
-    mVecStr :: String
+data Options = Options {
+    optIndices :: [Int],
+    optMaxAf :: Int,
+    optLambda :: [Double],
+    optJoins :: [Join],
+    optCommand :: Command
 }
 
-runWithOptions :: MyOpts -> IO ()
-runWithOptions opts = do
-    let nVec = map read (splitOn "," (nVecStr opts))
-        mVec = map read (splitOn "," (mVecStr opts))
-        k = length nVec
-        lambda = replicate k 1.0
-        joins = []
-        theta = 0.0005
-    print $ getProb defaultTimes lambda joins theta nVec mVec
+data Command = View | Prob [Int] [Int]
 
-main = OP.execParser opts >>= runWithOptions
-  where
-    parser = MyOpts <$> OP.argument OP.str (OP.metavar "NVec") <*> OP.argument OP.str (OP.metavar "MVec")
-    opts = OP.info parser mempty
+main :: IO ()
+main = run =<< OP.execParser (parseOptions `withInfo` "Rarecoal: Implementation of the Rarecoal algorithm")
+
+parseOptions :: OP.Parser Options
+parseOptions = Options <$> parseIndices <*> parseMaxAf <*> parseLambda <*> parseJoins <*> parseCommand
+
+parseIndices :: OP.Parser [Int]
+parseIndices = OP.option OP.auto $ OP.short 'i' <> OP.long "indices" <> OP.metavar "[i1,i2,...]"
+                                        <> OP.value []
+                                        <> OP.help "Read only these indices from the input file"
+
+parseMaxAf :: OP.Parser Int
+parseMaxAf = OP.option OP.auto $ OP.short 'm' <> OP.long "max_af" <> OP.metavar "INT"
+                                        <> OP.value 10
+                                        <> OP.help "set the maximum allele frequency"
+
+parseLambda :: OP.Parser [Double]
+parseLambda = OP.option OP.auto $ OP.short 'p' <> OP.long "pop_size" <> OP.metavar "[l1,l2,...]"
+                                        <> OP.value []
+                                        <> OP.help "Initial inverse population sizes"
+
+parseJoins :: OP.Parser [Join]
+parseJoins = OP.option OP.auto $ OP.short 'j' <> OP.long "joins" <> OP.metavar "[(t1,k1,l1,lambda1),(t2,k2,l2,lambda2),...]"
+                                        <> OP.value []
+                                        <> OP.help "Joins"
+
+parseCommand :: OP.Parser Command
+parseCommand = OP.subparser $
+    OP.command "view" (parseView `withInfo` "View the input file") <>
+    OP.command "prob" (parseProb `withInfo` "Compute probability for a given configuration")
+
+parseView :: OP.Parser Command
+parseView = pure View
+
+withInfo :: OP.Parser a -> String -> OP.ParserInfo a
+withInfo opts desc = OP.info (OP.helper <*> opts) $ OP.progDesc desc
+
+parseProb :: OP.Parser Command
+parseProb = Prob <$> OP.argument OP.auto (OP.metavar "NVec")
+                 <*> OP.argument OP.auto (OP.metavar "MVec")
+
+theta = 0.0005
+
+run :: Options -> IO ()
+run (Options indices max_af lambda joins cmd) = do
+    case cmd of
+        View -> print "View was selected"
+        Prob nVec mVec -> do
+            case getProb defaultTimes lambda joins theta nVec mVec of
+                Left err -> do
+                    putStrLn $ "Error: " ++ err
+                    exitFailure
+                Right result -> do
+                    print result
+    
+    
+    
