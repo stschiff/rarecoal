f0e9df926b66a23ca2f527741ca053385125e8ee
Stephan Schiffels <stephan.schiffels@mac.com>
2015-02-16 14:26:35 +0000

    added option to freqSum2Histogram to constrain histogram globally

--- freqSum2Histogram.hs
+++ freqSum2Histogram.hs
@@ -10,7 +10,13 @@ import Data.Int (Int64)
 import Control.Error.Script (scriptIO, runScript)
 import Control.Monad.Trans.Either (hoistEither)
 
-data MyOpts = MyOpts [Int] Int [Int] Int64
+data MyOpts = MyOpts {
+    optNVec:: [Int],
+    optMaxM :: Int,
+    optPopIndices :: [Int],
+    optNrCalledSites :: Int64,
+    globalMax :: Bool
+}
 
 main :: IO ()
 main = OP.execParser opts >>= runWithOptions
@@ -33,20 +39,24 @@ parser = MyOpts <$> OP.option OP.auto (OP.short 'n' <> OP.long "nVec" <> OP.meta
                                        <> OP.help "Specify the exact populations for the histogram")
                 <*> OP.option OP.auto (OP.long "nrCalledSites" <> OP.short 'N'
                                        <> OP.help "set the total nr of called sites")
+                <*> OP.option OP.auto (OP.long "globalMax" <> OP.short 'g' <> OP.value False <> OP.showDefault
+                                       <> OP.help "constrain global allele frequency")
 
 runWithOptions :: MyOpts -> IO ()
-runWithOptions (MyOpts nVec maxM popIndices nrCalledSites) = runScript $ do
-    let prod = P.stdinLn >-> P.map (mkPat maxM popIndices)
+runWithOptions (MyOpts nVec maxM popIndices nrCalledSites globalMax) = runScript $ do
+    let prod = P.stdinLn >-> P.map (mkPat maxM popIndices globalMax)
     res <- scriptIO $ P.fold insertPattern Map.empty id prod
     let hist = RareAlleleHistogram (selectFromList nVec popIndices) 0 maxM False res
     hist' <- hoistEither $ setNrCalledSites nrCalledSites hist 
     outs <- hoistEither $ showHistogram hist'
     scriptIO $ putStr outs
 
-mkPat :: Int -> [Int] -> String -> SitePattern
-mkPat maxM popIndices line =
+mkPat :: Int -> [Int] -> Bool -> String -> SitePattern
+mkPat maxM popIndices globalMax line =
     let pattern = selectFromList (fsCounts $ read line) popIndices
-    in if any (>maxM) pattern then Higher else Pattern pattern
+    in if filterPattern pattern then Higher else Pattern pattern
+  where
+    filterPattern = if globalMax then (>maxM) . sum else any (>maxM)
 
 insertPattern :: Map.Map SitePattern Int64 -> SitePattern -> Map.Map SitePattern Int64
 insertPattern m p = Map.insertWith (\_ v -> v + 1) p 1 m
