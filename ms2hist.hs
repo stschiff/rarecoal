7d5dd5ebbd643db93179e75e18da3ecc952c5bf0
Stephan Schiffels <stephan.schiffels@mac.com>
2015-02-22 13:45:26 +0100

    finished ms2hist.hs. Compiles, but not tested yet

--- ms2hist.hs
+++ ms2hist.hs
@@ -1,20 +1,23 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 import Data.List.Split (splitPlaces)
-import Control.Applicative ((<$>), (<*>))
+import Control.Applicative ((<$>), (<*>), pure)
 import Data.Monoid (mempty, (<>))
+import qualified Data.Map as M
 import qualified Options.Applicative as OP
-import RareAlleleHistogram(RareAlleleHistogram(..), showHistogram)
+import RareAlleleHistogram(RareAlleleHistogram(..), showHistogram, SitePattern(..))
 import Control.Lens (makeLenses, views, view)
 import Control.Error.Script (runScript, Script, scriptIO)
+import Control.Error.Safe (assertErr)
 import Control.Monad.Trans.Reader (runReaderT, ReaderT)
+import Control.Monad.Trans.Either (hoistEither)
 import Control.Monad.Trans.Class (lift)
+import Data.List (transpose)
 
 data MyOpts = MyOpts {
     _optNVec :: [Int],
     _optMaxM :: Int,
-    _optGlobalMax :: Bool,
-    _optFile :: FilePath
+    _optGlobalMax :: Bool
 }
 
 makeLenses ''MyOpts
@@ -25,17 +28,23 @@ main = OP.execParser opts >>= mainWithOptions
     parser = MyOpts <$> OP.option OP.auto (OP.short 'n' <> OP.long "nVec" <> OP.metavar "<LIST>")
                     <*> OP.option OP.auto (OP.short 'm' <> OP.long "maxM" <> OP.metavar "<INT>")
                     <*> OP.switch (OP.short 'g' <> OP.long "globalMax")
-                    <*> OP.argument OP.str (OP.metavar "<FILE>")
     opts = OP.info parser mempty
 
-type App = ReaderT MyOpts Script 
-
 mainWithOptions :: MyOpts -> IO ()
-mainWithOptions opts = runScript $ runReaderT run opts
+mainWithOptions (MyOpts nVec maxAf globalMax) = runScript $ do
+    s <- scriptIO getContents
+    hist <- hoistEither $ makeHist nVec maxAf globalMax s
+    outs <- hoistEither $ showHistogram hist
+    scriptIO $ putStrLn outs
 
-run :: App ()
-run = do
-    fn <- view optFile
-    s <- lift . scriptIO $ readFile fn
-    lift . scriptIO $ putStrLn s
-    
+makeHist :: [Int] -> Int -> Bool -> String -> Either String RareAlleleHistogram
+makeHist nVec maxAf global s = do
+    let loci = transpose . drop 6 . lines $ s
+    assertErr "nVec doesn't sum up to correct number of samples" $ length (head loci) == sum nVec
+    let getFreqSum = map (length . filter (=='1')) . splitPlaces nVec
+        freqSums = map getFreqSum loci
+        pred_ = if global then (<=maxAf) . sum else any (<=maxAf)
+        toPattern p = if pred_ p then Pattern p else Higher
+        insert m k = M.insertWith (+) k 1 m
+        counts = foldl insert M.empty $ map toPattern freqSums
+    return $ RareAlleleHistogram nVec 0 maxAf global counts
