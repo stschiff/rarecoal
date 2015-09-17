b127932e39bcf6782dcf0d81e74ddf9f94673934
Stephan Schiffels <stephan.schiffels@mac.com>
2015-09-17 21:46:29 +0100

    added some new tools

---
+++ groupFreqSum.hs
@@ -0,0 +1,33 @@
+import qualified Options.Applicative as OP
+import qualified Pipes.Text.IO as PT
+import Data.Monoid ((<>))
+import Pipes (runEffect, for, lift)
+import Pipes.Attoparsec (parsed)
+import Control.Error (runScript, left, tryRight, assertErr, scriptIO)
+import FreqSumEntry (FreqSumEntry(..), parseFreqSumEntry)
+import Data.List.Split (splitPlaces)
+import Control.Monad ((>=>))
+
+data MyOpts = MyOpts [Int]
+
+main :: IO ()
+main = OP.execParser opts >>= runWithOptions
+  where
+    opts = OP.info (OP.helper <*> parser) mempty
+
+parser :: OP.Parser MyOpts
+parser = MyOpts <$> OP.option OP.auto (OP.short 'n' <> OP.long "nVec" <> OP.metavar "nVec")
+
+runWithOptions :: MyOpts -> IO ()
+runWithOptions (MyOpts nVec) = runScript $ do
+    let freqSums = parsed parseFreqSumEntry PT.stdin
+    res <- runEffect $ for freqSums $ lift . tryRight . groupFreqSum nVec >=> lift . scriptIO . putStrLn . show
+    case res of
+        Left (err, _) -> left $ "Parsing error: " ++ show err
+        Right () -> return ()
+
+groupFreqSum :: [Int] -> FreqSumEntry -> Either String FreqSumEntry
+groupFreqSum nVec fs = do
+    assertErr "number of samples doesn't match nVec" $ sum nVec == length (fsCounts fs)
+    let groups = map sum . splitPlaces nVec $ fsCounts fs
+    return fs {fsCounts = groups}
--- vcf2FreqSum.hs
+++ vcf2FreqSum.hs
@@ -5,41 +5,28 @@ import qualified Pipes.Text.IO as PT
 import qualified Data.Text as T
 import Pipes.Group (folds)
 import Control.Foldl (purely, mconcat)
-import Data.List.Split (splitPlaces)
 import Control.Applicative ((<|>))
-import Data.Monoid ((<>))
-import qualified Options.Applicative as OP
-import Control.Error (assertErr, runScript, tryRight, Script)
+import Control.Error (runScript, tryRight, Script)
 import Control.Lens (view)
 import Data.Attoparsec.Text (Parser, char, decimal, letter, notInClass, parseOnly, sepBy1, takeWhile)
 import Prelude hiding (takeWhile, mconcat)
 import Control.Monad (replicateM_)
 import FreqSumEntry (FreqSumEntry(..))
 
-data MyOpts = MyOpts [Int]
-
 data VCFentry = VCFentry T.Text Int Char Char [(Char,Char)]
 
 main :: IO ()
-main = OP.execParser opts >>= runWithOptions
-  where
-    parser = MyOpts <$> OP.option OP.auto (OP.short 'n' <> OP.long "nVec" <> OP.metavar "NVec")
-    opts = OP.info parser mempty
-
-runWithOptions :: MyOpts -> IO ()
-runWithOptions (MyOpts nVec) =
-    runScript . runEffect $ lineProducer >-> P.filter (\l -> T.head l /= '#') >->
-                            P.mapM (tryRight . processVCFline nVec) >-> PT.stdout
+main = runScript . runEffect $ lineProducer >-> P.filter (\l -> T.head l /= '#') >->
+                               P.mapM (tryRight . processVCFline) >-> PT.stdout
 
 lineProducer :: Producer T.Text Script ()
 lineProducer = purely folds mconcat $ view PT.lines PT.stdin
 
-processVCFline :: [Int] -> T.Text -> Either String T.Text
-processVCFline nVec line = do
+processVCFline :: T.Text -> Either String T.Text
+processVCFline line = do
     VCFentry chrom pos ref alt genotypes <- parseOnly parseVCFentry line
-    let gens = concat [[g1, g2] | (g1, g2) <- genotypes]
-    assertErr "number of samples doesn't match nVec" $ 2 * sum nVec == length gens
-    let counts = [length $ filter (=='1') c | c <- splitPlaces (map (*2) nVec) gens]
+    let gens = [[g1, g2] | (g1, g2) <- genotypes]
+    let counts = [length $ filter (=='1') c | c <- gens]
         fs = FreqSumEntry (T.unpack chrom) pos ref alt counts
     return . flip T.snoc '\n' . T.pack . show $ fs
 
